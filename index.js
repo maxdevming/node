3df90acf8cc3dbe4efa59fed3ad3dfc4ac2c87a0
maxdevming <41966054+maxdevming@users.noreply.github.com>
2025-11-28 22:41:45 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+    const http = require('http'); // Import the built-in http module
+
+    const hostname = '44.210.69.137'; // Localhost
+    const port = 80; // Choose a port number
+
+    // Create the server
+    const server = http.createServer((req, res) => {
+      // Set the response HTTP header with a status code and content type
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+
+      // Send the response body
+      res.end('Hello World!\n');
+    });
+
+    // Start the server and listen for requests
+    server.listen(port, hostname, () => {
+      console.log(`Server running at http://${hostname}:${port}/`);
+    });
